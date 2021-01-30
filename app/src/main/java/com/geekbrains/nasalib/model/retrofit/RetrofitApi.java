0401cc24fa7913c473536d5edf2ed66ca16a97ca
Oleg <49286750+Daganep@users.noreply.github.com>
2021-01-30 17:59:09 +0300

    Retrofit change for Nasa request

--- app/src/main/java/com/geekbrains/nasalib/model/retrofit/RetrofitApi.java
+++ app/src/main/java/com/geekbrains/nasalib/model/retrofit/RetrofitApi.java
@@ -3,28 +3,34 @@ package com.geekbrains.nasalib.model.retrofit;
 import com.geekbrains.nasalib.model.entity.NasaResponse;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
-import io.reactivex.Observable;
+import io.reactivex.Single;
 import io.reactivex.schedulers.Schedulers;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitApi {
-    public Observable<NasaResponse> requestServer(){
+
+    private final String baseUrl = "https://images-api.nasa.gov/search";
+    private final String page = "1";
+    private final String searchKey = "apollo+11";
+    private RetrofitService api;
+
+    public RetrofitApi(){
         Gson gson = new GsonBuilder()
                 .excludeFieldsWithoutExposeAnnotation()
                 .create();
-
         GsonConverterFactory gsonConverterFactory = GsonConverterFactory.create(gson);
-
-        RetrofitService api = new Retrofit.Builder()
-                .baseUrl("https://images-api.nasa.gov/")
+        api = new Retrofit.Builder()
+                .baseUrl(baseUrl)
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(gsonConverterFactory)
                 .build()
                 .create(RetrofitService.class);
+    }
 
-        return api.getMedia().subscribeOn(Schedulers.io());
+    public Single<NasaResponse> requestServer(String searchKey, String page) {
+        return api.getMedia(searchKey, page)
+                .subscribeOn(Schedulers.io());
     }
 }
--- app/src/main/java/com/geekbrains/nasalib/model/retrofit/RetrofitService.java
+++ app/src/main/java/com/geekbrains/nasalib/model/retrofit/RetrofitService.java
@@ -2,10 +2,12 @@ package com.geekbrains.nasalib.model.retrofit;
 
 import com.geekbrains.nasalib.model.entity.NasaResponse;
 
-import io.reactivex.Observable;
+import io.reactivex.Single;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface RetrofitService {
-    @GET("planetary/apod?api_key=Tv0CsKjtovbqSrb6ehgvLHdfhlpkV1KWJa69XI0O")
-    Observable<NasaResponse> getMedia();
+    @GET("api")
+    Single<NasaResponse> getMedia(@Query("q") String searchKey,
+                                     @Query("page") String page);
 }
