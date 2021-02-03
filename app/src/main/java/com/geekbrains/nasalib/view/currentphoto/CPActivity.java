eb3513a548cb54f27992f573f25c575c83febd03
Oleg <49286750+Daganep@users.noreply.github.com>
2021-02-03 10:54:54 +0300

    Fix out of bound graphic. Change background picture.

--- app/src/main/java/com/geekbrains/nasalib/view/currentphoto/CPActivity.java
+++ app/src/main/java/com/geekbrains/nasalib/view/currentphoto/CPActivity.java
@@ -1,23 +1,15 @@
 package com.geekbrains.nasalib.view.currentphoto;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.databinding.DataBindingUtil;
-
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
-
 import com.geekbrains.nasalib.R;
 import com.geekbrains.nasalib.databinding.ActivityCPBinding;
 import com.geekbrains.nasalib.model.entity.CEInfo;
-import com.geekbrains.nasalib.model.entity.Item;
 import com.geekbrains.nasalib.model.picasso.ImageSetter;
 import com.geekbrains.nasalib.presenter.CPPresenter;
 
-import javax.inject.Inject;
+import java.util.Objects;
 
-import moxy.InjectViewState;
 import moxy.MvpAppCompatActivity;
 import moxy.presenter.InjectPresenter;
 
@@ -41,10 +33,13 @@ public class CPActivity extends MvpAppCompatActivity implements CPView {
     }
 
     public void setData(CEInfo ceInfo) {
+        Objects.requireNonNull(getSupportActionBar()).setTitle(ceInfo.getTitle());
+        String creator = "unknown";
+        if(ceInfo.getData() == null) creator = ceInfo.getData();
         imageSetter.setImage(ceInfo.getURL(), cpActivity.currentPhotoIV);
-        cpActivity.CPTitleTV.setText(ceInfo.getTitle());
-        cpActivity.CPCreatorTV.setText(ceInfo.getCreator());
-        cpActivity.CPDateTV.setText(ceInfo.getData());
+        cpActivity.CPTitleTV.setText(String.format("%s%s", getString(R.string.CP_title), ceInfo.getTitle()));
+        cpActivity.CPCreatorTV.setText(String.format("%s%s", getString(R.string.sec_creator), creator));
+        cpActivity.CPDateTV.setText(String.format("%s%s", getString(R.string.date_creation), ceInfo.getData()));
     }
 
     private void initToolbar() {
